9d35f051590809ce9385b02d499e6b1e2ff4f058
fedyhendrawan3 <fedyhendrawan3@gmail.com>
2022-10-06 21:25:35 +0700

    Kirim hasil Tugas WEB

---
+++ clean/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+ 
+     <!-- Site Metas -->
+    <title>CleanPhotography</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+	<script src="js/modernizr.js"></script> <!-- Modernizr -->
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body id="page-top" class="politics_version">
+
+    <!-- LOADER -->
+    <div id="preloader">
+		<div id="main-ld">
+			<div class="loader">
+				<span class="loader-inner">CleanPhotography</span>
+			</div>
+		</div>
+    </div><!-- end loader -->
+    <!-- END LOADER -->
+	
+	
+	<div class="fat-nav">
+        <div class="fat-nav__wrapper">
+            <ul>
+                <li><a class="js-scroll-trigger" href="#home">Home</a></li>
+                <li><a class="js-scroll-trigger" href="#about">About Me</a></li>
+                <li><a class="js-scroll-trigger" href="#gallery">Gallery</a></li>
+                <li><a class="js-scroll-trigger" href="#services">Service</a></li>
+				<li><a class="js-scroll-trigger" href="#reviews">Reviews</a></li>
+				<li><a class="js-scroll-trigger" href="#pricing">Pricing</a></li>
+				<li><a class="js-scroll-trigger" href="#contact">Contact Me</a></li>
+            </ul>
+        </div>
+    </div>
+	
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container-fluid">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+			<img class="img-fluid" src="images/logo.png" alt="" />
+		</a>
+      </div>
+    </nav>
